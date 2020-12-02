549f15bfdf67f5fbcfac0d188a83b7d758a66a63
Merven-laptop <w1270107143@qq.com>
2020-12-02 16:35:11 +0800

    quant tool by Merven

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using  JQData.Samples;
+using System.Data;
+
+namespace quantTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void queryButton_Click(object sender, RoutedEventArgs e)
+        {
+            displayBlock.DataContext = null;
+
+            Dictionary<string, string> showMsg = getShowMsg();
+            if(showMsg != null)
+            {
+                try
+                {
+                    displayBlock.Text = DateTime.Now.ToString() +  "\n"
+                     + "当前价格:\t" + showMsg["current_price"] + "\n" 
+                     + "历史最高价格:\t" + showMsg["highest_price"] + "\n"
+                     + "历史最低价格:\t" + showMsg["lowest_price"];
+                }
+                catch
+                {
+                    displayBlock.Text = "error";
+                }
+            }
+            else
+            {
+                displayBlock.Text = "error";
+            }
+
+
+        }
+
+        private Dictionary<string, string> getShowMsg()
+        {
+            string period = '1' + periodType.SelectedItem.ToString().Split(':')[1].Trim();
+            string numStr = periodNum.Text;
+            try
+            {
+                int numVal;
+                numVal = Convert.ToInt32(numStr);
+            }
+            catch
+            {
+                return null;
+            }
+            string paramStr = "--period=" + period + "  --periodNum=" + numStr;
+            Dictionary<string, string> queryRes = 
+                QuantQuery.QuerySecurityInfo(paramStr);
+            return queryRes;
+        }
+    }
+}
---
+++ ref/QuantQuery.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+
+namespace JQData.Samples
+{
+    class QuantQuery
+    {
+        public static Dictionary<string, string> QuerySecurityInfo(string pyParam,
+            string exePath = @"F:\temp_code\biquant\mv_programe.exe")
+        {
+            /* parameters example:
+             * paramStr = "--period=1day  --periodNum=15";
+             * pyexePath = @"F:\temp_code\biquant\mv_programe.exe";
+             * 
+             *  return example:
+             *  btc-usdt result:
+             *  current-time: Wed Dec  2 14:24:50 2020
+             *  current_price: 18774.81
+             *  highest-price_in_past_7_day:19888.0$
+             *  lowest-price_in_past_7_day:16199.86$
+             */
+            Dictionary<string, string> res = new Dictionary<string, string>();
+
+            Process p = new Process();
+            p.StartInfo.FileName = exePath;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = pyParam;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+            //Console.WriteLine(output);
+            //Console.WriteLine("test the info:");
+            //Console.WriteLine(output.Length);
+
+            if (output.StartsWith("fail"))
+            {
+                return res;
+            }
+            else
+            {
+                string[] resList = output.Split("\n");
+                for (int i = 1; i < resList.Length; i++)
+                {
+                    string[] lineElem;
+                    if (resList[i].Length > 0)
+                    {
+                        lineElem = resList[i].Split(":");
+                        if (lineElem.Length > 0)
+                            res[lineElem[0]] = lineElem[1];
+                    }
+                }
+                return res;
+            }
+        }
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paramStr = "--period=1day  --periodNum=15";
+            string pyexePath = @"F:\temp_code\biquant\mv_programe.exe";
+            Dictionary<string, string> queryRes = QuantQuery.QuerySecurityInfo(paramStr, pyexePath);
+            foreach(string keyElem in queryRes.Keys)
+            {
+                Console.WriteLine(keyElem + " :  " + queryRes[keyElem]);
+            }
+        }
+
+    }
+}
